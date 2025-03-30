cc3190e1ad50f83a42ba132dd6d552cbce2fd36f
DIANA <75088804@continental.edu.pe>
2025-03-29 20:36:01 -0500

    Add files via upload

---
+++ ejercicio2.py
@@ -0,0 +1,13 @@
+#Elabora el volumen de un cubo, solicitar ingreso de datos.
+#Objetivo: Volumen de un cubo
+#Nombre: Diana Quispe Bendezu
+#Fecha: 29/03/25
+
+#ENTRADA 
+LADO=int(input("Ingrese el lado: "))
+
+#PROCESO
+Calcular = LADO ** 3
+
+#SALIDA
+print("El volumen con los datos ingresados es: ", Calcular)
