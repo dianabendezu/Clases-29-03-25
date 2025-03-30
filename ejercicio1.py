8cb8a1eb8fa85f21263f9114f1ed97f9576fffdb
DIANA <75088804@continental.edu.pe>
2025-03-29 20:00:46 -0500

    Add files via upload

---
+++ ejercicio1.py
@@ -0,0 +1,15 @@
+#Elabora un programa para calcular el área de un rectángulo, solicitar ingreso de datos.
+#Objetivo
+#Nombre: Diana Quispe Bendezu
+#Fecha: 29/03/25
+
+#ENTRADA
+base=int(input("Ingrese la base: "))
+altura=int(input("Ingrese la altura: "))
+
+#PROCESO
+Calcular = base * altura
+
+#SALIDA
+print("El area del rectangulo con los datos ingresados es: ", Calcular)
+
